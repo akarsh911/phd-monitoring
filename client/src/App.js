0f5c23eaa823fa316a4f0413d4353dc8cf992c71
himanishpuri <himanishpuri2203@gmail.com>
2024-10-09 12:30:31 +0530

    improved the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,44 @@
 import React from "react";
 import Dashboard from "./pages/dashboard/Dashboard";
 import SupDashboard from "./pages/dashboard/SupDashboardTemp";
-import Login from "./pages/login/MacBookPro14";
+import LoginPage from "./pages/login/LoginPage.js";
 import Home from "./pages/home/homepage";
-import ConstituteofIrb from "./pages/forms/ConstituteOfIrb/ConstituteOfIrb"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import IrbSup from "./pages/forms/IrbSubmissionSup";
-import Forms from "./pages/dashboard/Forms";
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  return (
-    <div>
-       <ToastContainer />
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/students" element={<SupDashboard type="students" />} />
-          <Route path="/dashboard/forms/:id" element={<SupDashboard type="form" />} />
-      
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<ToastContainer />
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						exact
+						element={<Home />}
+					/>
+					<Route
+						path="/login"
+						element={<LoginPage />}
+					/>
+					<Route
+						path="/dashboard"
+						element={<Dashboard />}
+					/>
+					<Route
+						path="/dashboard/students"
+						element={<SupDashboard type="students" />}
+					/>
+					<Route
+						path="/dashboard/forms/:id"
+						element={<SupDashboard type="form" />}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
