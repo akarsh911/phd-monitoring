22dcb06f628bb5d96b181eea17da97e8a715679b
Akarsh Srivastava <akarsh91140@gmail.com>
2024-11-06 00:48:55 +0530

    Fix STUDENT_PROFILE endpoint URL by removing trailing slash

--- client-new/src/api/urls.js
+++ client-new/src/api/urls.js
@@ -2,5 +2,5 @@ export const baseURL= 'http://api-phd.ccstiet.com/api';
 
 export const ENDPOINTS = {
     LOGIN: `${baseURL}/login`,
-    STUDENT_PROFILE: `${baseURL}/students/`,
+    STUDENT_PROFILE: `${baseURL}/students`,
 }
