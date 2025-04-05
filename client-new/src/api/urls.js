b489630780a9262dcc2a0638beb31db3b1c9275e
Akarsh Srivastava <akarsh91140@gmail.com>
2025-04-05 22:55:54 +0530

    Remove deprecated urls.js file and its associated API endpoints

--- client-new/src/api/urls.js
+++
@@ -1,6 +0,0 @@
-export const baseURL= 'https://phdportal.thapar.edu/api/api';
-// export const baseURL= 'http://localhost:8000/api';
-export const ENDPOINTS = {
-    LOGIN: `${baseURL}/login`,
-    STUDENT_PROFILE: `${baseURL}/students`,
-}
