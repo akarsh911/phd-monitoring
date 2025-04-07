31124ef37c3b16f5f50f61bfe6ff98281c3c090e
Akarsh Srivastava <akarsh91140@gmail.com>
2025-04-07 18:33:53 +0530

    Refactor password reset routes to use environment variable for frontend URL

--- server/routes/web.php
+++ server/routes/web.php
@@ -8,11 +8,12 @@ Route::get('/', function () {
 });
 
 
-
 Route::get('/reset-password/{token}', function ($token) {
-    return redirect("/reset-password?token=$token");
+    $url = env('FRONTEND_URL', 'http://localhost:3000') . "/reset-password?token=$token";
+    return redirect($url);
 })->name('password.reset');
 
 Route::get('api/reset-password/{token}', function ($token) {
-    return redirect("/reset-password?token=$token");
+    $url = env('FRONTEND_URL', 'http://localhost:3000') . "/reset-password?token=$token";
+    return redirect($url);
 })->name('password.reset.redirect');
