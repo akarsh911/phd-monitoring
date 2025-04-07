cc4b87669ce860fdc9e334d41f986c19aac870cb
Akarsh Srivastava <akarsh91140@gmail.com>
2025-04-07 18:30:48 +0530

    Add API route for password reset redirection

--- server/routes/web.php
+++ server/routes/web.php
@@ -12,3 +12,7 @@ Route::get('/', function () {
 Route::get('/reset-password/{token}', function ($token) {
     return redirect("/reset-password?token=$token");
 })->name('password.reset');
+
+Route::get('api/reset-password/{token}', function ($token) {
+    return redirect("/reset-password?token=$token");
+})->name('password.reset.redirect');
