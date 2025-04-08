c30c31a4b740638b148e2d5ed682c7fdc4b60968
Gurman <gurmankaur0603@gmail.com>
2025-04-08 12:14:34 +0530

    changes

--- client-new/src/components/forms/listOfExaminers/roles/Supervisor.js
+++ client-new/src/components/forms/listOfExaminers/roles/Supervisor.js
@@ -1,144 +1,166 @@
-import React, { useState } from "react";
-import CustomButton from "../../fields/CustomButton";
-import GridContainer from "../../fields/GridContainer";
-import { baseURL } from "../../../../api/urls";
-import InputSuggestions from "../../fields/InputSuggestions";
-import TableComponent from "../../table/TableComponent";
-import CustomModal from "../../modal/CustomModal";
-import AddExaminer from "../../AddExaminer/AddExaminer";
-import { customFetch } from "../../../../api/base";
-import { useLoading } from "../../../../context/LoadingContext";
-import { useLocation } from "react-router-dom";
-import RadioButtonGroup from "../../fields/RadioButtonGroup";
+import React, { useState } from 'react';
+import CustomButton from '../../fields/CustomButton';
+import GridContainer from '../../fields/GridContainer';
+import { baseURL } from '../../../../api/urls';
+import InputSuggestions from '../../fields/InputSuggestions';
+import TableComponent from '../../table/TableComponent';
+import CustomModal from '../../modal/CustomModal';
+import AddExaminer from '../../AddExaminer/AddExaminer';
+import { customFetch } from '../../../../api/base';
+import { useLoading } from '../../../../context/LoadingContext';
+import { useLocation } from 'react-router-dom';
+import RadioButtonGroup from '../../fields/RadioButtonGroup';
 
-const ExaminerManager = ({ type, formData,examData, apiUrl, onAddExaminer }) => {
-    const [examiners, setExaminers] = useState(examData || []);
-    const [modalData, setModalData] = useState({});
-    const [isModalOpen, setIsModalOpen] = useState(false);
-     
-    const handleSearchSelect = (selectedExaminer) => {
-        setModalData(selectedExaminer);
-        setIsModalOpen(true);
-    };
+const ExaminerManager = ({
+  type,
+  formData,
+  examData,
+  apiUrl,
+  onAddExaminer,
+}) => {
+  const [examiners, setExaminers] = useState(examData || []);
+  const [modalData, setModalData] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleOpenModal = () => {
-        setModalData({});
-        setIsModalOpen(true);
-    };
+  const handleSearchSelect = (selectedExaminer) => {
+    setModalData(selectedExaminer);
+    setIsModalOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+  const handleOpenModal = () => {
+    setModalData({});
+    setIsModalOpen(true);
+  };
 
-    const handleAddExaminer = (examiner) => {
-        setExaminers([...examiners, examiner]);
-        onAddExaminer(examiner);
-        setIsModalOpen(false);
-    };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <div>
-            <CustomModal
-                isOpen={isModalOpen}
-                onClose={handleCloseModal}
-                minWidth="700px"
-                maxWidth="800px"
-                minHeight="300px"
-                maxHeight="500px"
-            >
-                <AddExaminer data={modalData} onSubmit={handleAddExaminer} />
-            </CustomModal>
-            {console.log(formData)}
-      {!formData.locks.supervisor && formData.role==='supervisor' && (<>
-            <GridContainer
-                elements={[
-                    <InputSuggestions
-                        apiUrl={apiUrl}
-                        hint="Search here..."
-                        onSelect={handleSearchSelect}
-                        label=""
-                        showLabel={false}
-                    />,
-                    <CustomButton
-                        text={`Add New ${type}`}
-                        onClick={handleOpenModal}
-                    />,
+  const handleAddExaminer = (examiner) => {
+    setExaminers([...examiners, examiner]);
+    onAddExaminer(examiner);
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <CustomModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        minWidth='700px'
+        maxWidth='800px'
+        minHeight='300px'
+        maxHeight='500px'
+      >
+        <AddExaminer data={modalData} onSubmit={handleAddExaminer} />
+      </CustomModal>
+      {console.log(formData)}
+      {!formData.locks.supervisor && formData.role === 'supervisor' && (
+        <>
+          <GridContainer
+            elements={[
+              <InputSuggestions
+                apiUrl={apiUrl}
+                hint='Search here...'
+                onSelect={handleSearchSelect}
+                label=''
+                showLabel={false}
+              />,
+              <CustomButton
+                text={`Add New ${type}`}
+                onClick={handleOpenModal}
+              />,
+            ]}
+            ratio={[2, 1]}
+            space={2}
+            label={`${type} Examiners`}
+          />
+        </>
+      )}
+      {formData.locks.dordc && (
+        <>
+          {' '}
+          <GridContainer
+            elements={[
+              <TableComponent
+                data={examiners}
+                titles={[
+                  'Name',
+                  'Email',
+                  'Department',
+                  'Designation',
+                  'Institution',
+                  'Status',
                 ]}
-                ratio={[2, 1]}
-                space={2}
-                label={`${type} Examiners`}
-            />
-            </>
-        )}
-        {
-            formData.locks.dordc && (<> <GridContainer
-                elements={[
-                    <TableComponent
-                        data={examiners}
-                        titles={["Name", "Email", "Department", "Designation", "Institution", "Status"]}
-                        keys={["name", "email", "department", "designation", "institution", "recommendation"]}
-                    />,
+                keys={[
+                  'name',
+                  'email',
+                  'department',
+                  'designation',
+                  'institution',
+                  'recommendation',
                 ]}
-                space={3}
-            />
-            </>)
-        }
-        </div>
-    );
+              />,
+            ]}
+            space={3}
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
 const Supervisor = ({ formData }) => {
-    const [national, setNational] = useState(formData.national || []);
-    const [international, setInternational] = useState(formData.international || []);
-    const { setLoading } = useLoading();
-      const location = useLocation();
-    const handleAddNationalExaminer = (examiner) => {
-        console.log("National Examiner Added:", examiner);
-        setNational([...national, examiner]);
-    };
+  const [national, setNational] = useState(formData.national || []);
+  const [international, setInternational] = useState(
+    formData.international || []
+  );
+  const { setLoading } = useLoading();
+  const location = useLocation();
+  const handleAddNationalExaminer = (examiner) => {
+    console.log('National Examiner Added:', examiner);
+    setNational([...national, examiner]);
+  };
 
-    const handleAddInternationalExaminer = (examiner) => {
-        console.log("International Examiner Added:", examiner);
-        setInternational([...international, examiner]);
-    };
+  const handleAddInternationalExaminer = (examiner) => {
+    console.log('International Examiner Added:', examiner);
+    setInternational([...international, examiner]);
+  };
 
-    const submitExaminers = () => {
-         setLoading(true);
-            customFetch(baseURL + location.pathname , "POST", {
-            national: national,
-            international: international,
-        })  .then((data) => {
-            if (data && data.success) {
-                console.log("Examiners submitted successfully");
+  const submitExaminers = () => {
+    setLoading(true);
+    customFetch(baseURL + location.pathname, 'POST', {
+      national: national,
+      international: international,
+    }).then((data) => {
+      if (data && data.success) {
+        console.log('Examiners submitted successfully');
+      }
+      setLoading(false);
+    });
+  };
 
-            }
-            setLoading(false);
-        });
-    };
-
-    return (
-        <div>
-            <ExaminerManager
-                type="National"
-                formData={formData}
-                examData={formData.national}
-                apiUrl={`${baseURL}/suggestions/examiner`}
-                onAddExaminer={handleAddNationalExaminer}
-            />
-            <ExaminerManager
-                type="International"
-                formData={formData}
-                examData={formData.international}
-                apiUrl={`${baseURL}/suggestions/examiner`}
-                onAddExaminer={handleAddInternationalExaminer}
-            />
-            <GridContainer
-                elements={[
-                    <CustomButton text="Submit" onClick={submitExaminers}/>,
-                ]}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <ExaminerManager
+        type='National'
+        formData={formData}
+        examData={formData.national}
+        apiUrl={`${baseURL}/suggestions/examiner`}
+        onAddExaminer={handleAddNationalExaminer}
+      />
+      <ExaminerManager
+        type='International'
+        formData={formData}
+        examData={formData.international}
+        apiUrl={`${baseURL}/suggestions/examiner`}
+        onAddExaminer={handleAddInternationalExaminer}
+      />
+      <GridContainer
+        elements={[<CustomButton text='Submit' onClick={submitExaminers} />]}
+      />
+    </div>
+  );
 };
 
 export default Supervisor;
