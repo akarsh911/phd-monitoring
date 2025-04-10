1d724265eb33067a58a8e27e433b0ba3e347788c
Akarsh Srivastava <akarsh91140@gmail.com>
2025-04-10 15:41:59 +0530

    refactor: Update route structure in presentation.php to consolidate form and semester endpoints

--- server/routes/base/presentation.php
+++ server/routes/base/presentation.php
@@ -10,25 +10,21 @@ Route::middleware('auth:sanctum')->group(function () {
     // Filters
     Route::get('/filters', [PresentationController::class, 'listFilters']);
     Route::get('/form/filters', [PresentationController::class, 'listFilters']);
+    
+    // Bulk Scheduling
+    Route::get('/semester/{semester_id}', [PresentationController::class, 'listForm']);
+    Route::post('/semester/{semester_id}', [PresentationController::class, 'createForm']);
+    Route::post('/semester/{semester_id}/bulk-schedule', [PresentationController::class, 'createMultipleForm']);
     Route::get('/semester/{semester_id}/filters', [PresentationController::class, 'listFilters']);
 
-    // Bulk Scheduling
-    Route::post('/form/bulk-schedule', [PresentationController::class, 'createMultipleForm']);
-    Route::get('/form/bulk-schedule', [PresentationController::class, 'createMultipleForm']);
-
-    // Form Listing & Creation
-    Route::get('/form', [PresentationController::class, 'listForm']);
-    Route::post('/form', [PresentationController::class, 'createForm']);
-
     // Form with ID (view, submit, link/unlink)
-    Route::get('/form/{form_id}', [PresentationController::class, 'loadForm']);
-    Route::post('/form/{form_id}', [PresentationController::class, 'submit']);
-    Route::post('/form/{form_id}/link', [PresentationController::class, 'linkPublication']);
-    Route::post('/form/{form_id}/unlink', [PresentationController::class, 'unlinkPublication']);
+    Route::get('/semester/{semester_id}/{form_id}', [PresentationController::class, 'loadForm']);
+    Route::post('/semester/{semester_id}/{form_id}', [PresentationController::class, 'submit']);
+    Route::post('/semester/{semester_id}/{form_id}/link', [PresentationController::class, 'linkPublication']);
+    Route::post('/semester/{semester_id}/{form_id}/unlink', [PresentationController::class, 'unlinkPublication']);
 
     // Bulk form submission
-    Route::post('/form/bulk', [PresentationController::class, 'bulkSubmit']);
+    Route::post('/semester/{semester_id}/bulk', [PresentationController::class, 'bulkSubmit']);
 
     // Semester-based form listing
-    Route::get('/semester/{semester_id}', [PresentationController::class, 'listForm']);
 });
