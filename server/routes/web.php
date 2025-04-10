824184969d2f7e9ba0f46c1eed68778326b9469d
Akarsh Srivastava <akarsh91140@gmail.com>
2025-04-11 02:29:44 +0530

    fix: Update frontend URL for password reset routes to production environment

--- server/routes/web.php
+++ server/routes/web.php
@@ -9,11 +9,11 @@ Route::get('/', function () {
 
 
 Route::get('/reset-password/{token}', function ($token) {
-    $url = env('FRONTEND_URL', 'http://localhost:3000') . "/reset-password?token=$token";
+    $url = env('FRONTEND_URL', 'https://phdportal.thapar.edu') . "/reset-password?token=$token";
     return redirect($url);
 })->name('password.reset');
 
 Route::get('api/reset-password/{token}', function ($token) {
-    $url = env('FRONTEND_URL', 'http://localhost:3000') . "/reset-password?token=$token";
+    $url = env('FRONTEND_URL', 'https://phdportal.thapar.edu') . "/reset-password?token=$token";
     return redirect($url);
 })->name('password.reset.redirect');
